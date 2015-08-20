1de823337403956e256e2527f0a11c2255b1ad8e
Suresh C <cherukuri7@yahoo.com>
2015-08-19 20:24:49 -0500

    Raw code without descriptive comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+        m <-NULL
+        set <- function (y){ 
+                check <- class(try(solve(y),silent=T))=="matrix"
+                if(check==TRUE){
+                        x <<- y      
+                        m <<- NULL}
+                else{
+                        stop("Matrix must be squre")}
+                
+        }
+        get <- function() x
+        setInv <- function(solve) m <<- solve
+        getInv <- function() m
+        
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInv(m)
+        m
 }
